714d052d76d4053abb849119ae1f7a1304a4fe6d
gbucci <gbucci@uol.com.br>
2019-04-15 11:40:33 -0300

    cookie no guard

--- tcc-web/src/app/guards/auth-guard.service.ts
+++ tcc-web/src/app/guards/auth-guard.service.ts
@@ -1,20 +1,26 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private cookieService: CookieService) { }
 
   private temPermissao = false;
 
+  private cookieValue;
+  
   canActivate() {
+    this.cookieValue = this.cookieService.get('Test');
 
-    if (!this.temPermissao)
-      return this.router.navigate(['/login']);
+  console.log(this.cookieValue);
 
-  }
+  if (!this.temPermissao)
+    return this.router.navigate(['/login']);
+
+}
 
 }
