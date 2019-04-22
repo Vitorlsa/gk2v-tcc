4a3bcc321869fb79d3db2eeac67ce19901c8c41d
gbuccir <gbucci@uol.com.br>
2019-04-21 22:31:35 -0300

    analise listagem e esclusao

--- tcc-web/src/app/modulos/board/analise-cadastro/analise-cadastro.component.ts
+++ tcc-web/src/app/modulos/board/analise-cadastro/analise-cadastro.component.ts
@@ -10,13 +10,20 @@ export class AnaliseCadastroComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
 
-  public cadastros = [];
-  public cadastrado = { nome: "Orandi", desc: ["enfermeiro", "cuidador"] };
-  private api = "";
+  public gestores = null;
+  public contratantes = null;
+  public prestadores = null;
+  // public cadastrado = { nome: "Orandi", desc: ["enfermeiro", "cuidador"] };
+  private apiContratante = "http://localhost:8080/api/contratante/listarnaoaprovados";
+  private apiPrestador = "http://localhost:8080/api/prestadordeservico/listarnaoaprovados";
+  private apiGestor = "http://localhost:8080/api/gestor/listarnaoaprovados";
+  private apiDeletar = "http://localhost:8080/api/usuario/deletar"
 
   ngOnInit() {
-    this.cadastros.push(this.cadastrado);
-    this.listarPendencias();
+    //this.cadastros.push(this.cadastrado);
+    this.listarContratante();
+    this.listarGestor();
+    this.listarPrestador();
   }
 
   aprovar(cadastro) {
@@ -27,9 +34,52 @@ export class AnaliseCadastroComponent implements OnInit {
     console.log("reprovado" + cadastro);
   }
 
-  listarPendencias() {
+  deletarUser(id, tipo) {
     try {
-      this.http.post(this.api, {}).subscribe(data => {
+      this.http.post(this.apiDeletar, { Id: id }).subscribe(data => {
+        console.log(data);
+        if (tipo == 1)
+          this.listarContratante();
+        else if (tipo == 2)
+          this.listarGestor();
+        else if (tipo == 3)
+          this.listarPrestador();
+      })
+    } catch{
+      console.log("nao chamaou api");
+    }
+
+
+
+  }
+
+
+  listarContratante() {
+    try {
+      this.http.post(this.apiContratante, {}).subscribe(data => {
+        this.contratantes = data;
+        console.log(data);
+      })
+    } catch{
+      console.log("nao chamaou api");
+    }
+  }
+
+  listarGestor() {
+    try {
+      this.http.post(this.apiGestor, {}).subscribe(data => {
+        this.gestores = data;
+        console.log(data);
+      })
+    } catch{
+      console.log("nao chamaou api");
+    }
+  }
+
+  listarPrestador() {
+    try {
+      this.http.post(this.apiPrestador, {}).subscribe(data => {
+        this.prestadores = data;
         console.log(data);
       })
     } catch{
