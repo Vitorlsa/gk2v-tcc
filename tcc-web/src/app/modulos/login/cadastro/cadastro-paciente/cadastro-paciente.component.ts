09673ec8a3e90d29fe28c3b25c748a9b3421413b
giuliano_clastech <giuliano@clastech.com.br>
2019-05-08 17:46:30 -0300

    editar do usuario

--- tcc-web/src/app/modulos/login/cadastro/cadastro-paciente/cadastro-paciente.component.ts
+++ tcc-web/src/app/modulos/login/cadastro/cadastro-paciente/cadastro-paciente.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CadastroServiceService } from '../cadastro-service.service';
 import { Paciente } from 'src/app/classes/paciente';
@@ -6,6 +6,7 @@ import { LoginService } from '../../login.service';
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
 import { DatePipe } from '@angular/common';
+import { UtilsService } from 'src/app/funcoes/utils.service';
 registerLocaleData(localePt);
 
 @Component({
@@ -14,59 +15,68 @@ registerLocaleData(localePt);
   styleUrls: ['./cadastro-paciente.component.scss']
 })
 export class CadastroPacienteComponent implements OnInit {
-  
 
-  constructor(private http:HttpClient, private service: LoginService, private cadastroService: CadastroServiceService) {
+
+  constructor(private http: HttpClient, private service: LoginService, private cadastroService: CadastroServiceService, private util:UtilsService) {
     this.paciente.sexo = 3;
-   }
+  }
 
-private apiSalvar = "http://localhost:8080/api/beneficiario/adicionar";
-private apiListar = "http://localhost:8080/api/beneficiario/listarporcontratante";
-private contratante = this.service.getUsuario();
-public paciente = new Paciente();
-public novoPaciente = false;
-public todosPacientes = null;
-public dataFormatada = null;
+  private apiSalvar = "http://localhost:8080/api/beneficiario/adicionar";
+  private apiListar = "http://localhost:8080/api/beneficiario/listarporcontratante";
+  private contratante = this.service.getUsuario();
+  public paciente = new Paciente();
+  public novoPaciente = false;
+  public todosPacientes = null;
+  public dataFormatada = null;
+  public perfilPaciente = null
+  //public pacienteSelecionado = null;
+
+
+  @Output()
+  emitir = new EventEmitter<any>();
+  pacienteSelecionado = null;
 
   ngOnInit() {
     this.limparPaciente();
 
     this.listarPacientes();
 
+    this.perfilPaciente = this.service.getSessionPerfil();
+
     var $window = $(window),
-			$header = $('#header'),
-			$footer = $('#footer');
+      $header = $('#header'),
+      $footer = $('#footer');
 
-		// Header.
-		$header.each(function () {
-			var t = jQuery(this),
-				button = t.find('.button');
-			button.click(function (e) {
-				t.toggleClass('hide');
-				if (t.hasClass('preview')) {
-					return true;
-				} else {
-					e.preventDefault();
-				}
-			});
-		});
+    // Header.
+    $header.each(function () {
+      var t = jQuery(this),
+        button = t.find('.button');
+      button.click(function (e) {
+        t.toggleClass('hide');
+        if (t.hasClass('preview')) {
+          return true;
+        } else {
+          e.preventDefault();
+        }
+      });
+    });
 
-		$footer.each(function () {
-			var t = jQuery(this),
-				inner = t.find('.inner'),
-				button = t.find('.info');
-			button.click(function (e) {
-				t.toggleClass('show');
-				e.preventDefault();
-			});
+    $footer.each(function () {
+      var t = jQuery(this),
+        inner = t.find('.inner'),
+        button = t.find('.info');
+      button.click(function (e) {
+        t.toggleClass('show');
+        e.preventDefault();
+      });
 
-		});
+    });
   }
 
 
   listarPacientes() {
     try {
-      this.http.post(this.apiListar, {Id: this.contratante.id }).subscribe(data => {
+      this.http.post(this.apiListar, { Id: this.contratante.id }).subscribe(data => {
         console.log(data);
         this.todosPacientes = [];
         console.log(typeof data);
@@ -74,35 +84,40 @@ public dataFormatada = null;
         this.todosPacientes.forEach((element, index) => {
           this.todosPacientes[index].dataFormatada = new DatePipe('pt-BR').transform(element.dataNascimento, 'dd/MM/yyyy');
         });
-         
+
       })
     } catch{
       console.log("nao chamaou api");
     }
   }
 
-  editarPaciente(param){
+  editarPaciente(param) {
     console.log(param);
+    this.pacienteSelecionado = param;
+    console.log(!this.util.nullOrUndef(this.pacienteSelecionado));
+    this.emitir.emit(this.pacienteSelecionado);
+    $('.modal-open').prop('checked', true);
   }
 
 
 
-criarNovoPaciente(){
-  this.novoPaciente = true;
-}
 
-voltar(){
-  this.limparPaciente();
-  this.novoPaciente = false;
-}
+  criarNovoPaciente() {
+    this.novoPaciente = true;
+  }
 
-setSexo(event) {
-  this.paciente.setSexo(event.target.value);
-}
+  voltar() {
+    this.limparPaciente();
+    this.novoPaciente = false;
+  }
 
-setTermos(event) {
-  this.paciente.setTermos(event.target.value);
-}
+  setSexo(event) {
+    this.paciente.setSexo(event.target.value);
+  }
+
+  setTermos(event) {
+    this.paciente.setTermos(event.target.value);
+  }
 
   salvar() {
     console.log(this.paciente);
@@ -121,26 +136,26 @@ setTermos(event) {
     }
   }
 
-  limparPaciente() {    
-    this.paciente.nome= "";
+  limparPaciente() {
+    this.paciente.nome = "";
     // this.paciente.login= "";
     // this.paciente.senha= "";
     // this.paciente.confirmaSenha= "";
-    this.paciente.email= "";
-    this.paciente.datanascimento= null;
-    this.paciente.sexo= 0;
-    this.paciente.cpf= "";
-    this.paciente.telefone= "";
-    this.paciente.cidade="";
-    this.paciente.estado= "";
-    this.paciente.bairro= "";
-    this.paciente.cep= "";
-    this.paciente.rua= "";
-    this.paciente.numero= "";
-    this.paciente.complemento= "";
-    this.paciente.comentario= "";
+    this.paciente.email = "";
+    this.paciente.datanascimento = null;
+    this.paciente.sexo = 0;
+    this.paciente.cpf = "";
+    this.paciente.telefone = "";
+    this.paciente.cidade = "";
+    this.paciente.estado = "";
+    this.paciente.bairro = "";
+    this.paciente.cep = "";
+    this.paciente.rua = "";
+    this.paciente.numero = "";
+    this.paciente.complemento = "";
+    this.paciente.comentario = "";
     //this.paciente.termos= false;
     this.paciente.condicoesClinicas = "";
-    
+
   }
 }
--- tcc-web/src/app/modulos/modal/modal-editar-paciente/modal-editar-paciente.component.ts
+++ tcc-web/src/app/modulos/modal/modal-editar-paciente/modal-editar-paciente.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+import { DatePipe } from '@angular/common';
+registerLocaleData(localePt);
 
 @Component({
   selector: 'app-modal-editar-paciente',
@@ -7,10 +12,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalEditarPacienteComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private apiBuscar = "http://localhost:8080/api/beneficiario/buscarporid";
+  private apiSalvar = "http://localhost:8080/api/beneficiario/editar";
+  public pacienteCompleto = null;
+
+
+  @Input()
+  pacienteSelecionado
 
   ngOnInit() {
-    console.log("modal editar paciente");
+    this.pegarInfosBeneficiario(this.pacienteSelecionado);
   }
+
+  pegarInfosBeneficiario(param) {
+    try {
+      this.http.post(this.apiBuscar, { Id: param.id }).subscribe(data => {
+        console.log(data);
+        this.pacienteCompleto = data;
+        this.pacienteCompleto.dataFormatada = new DatePipe('pt-BR').transform(this.pacienteCompleto.dataNascimento, 'dd/MM/yyyy');
+        if (this.pacienteCompleto.sexo == 1)
+          this.pacienteCompleto.sexo = "masculino";
+        else if (this.pacienteCompleto.sexo == 2)
+          this.pacienteCompleto.sexo = "feminino";
+        else
+          this.pacienteCompleto.sexo = "outro";
+      })
+    } catch{
+      console.log("nao chamaou api");
+    }
+  }
+
+  atualizarBeneficiario(param) {
+    param.dataNascimento = param.dataFormatada;
+    if (this.pacienteCompleto.sexo == "masculino")
+      this.pacienteCompleto.sexo = 1;
+    else if (this.pacienteCompleto.sexo == "feminino")
+      this.pacienteCompleto.sexo = 2
+    else
+      this.pacienteCompleto.sexo = 3;
+
+
+
+    try {
+      this.http.post(this.apiSalvar, param).subscribe(
+        res => {
+          alert("Cadastro salvo com sucesso");
+        },
+        err => {
+          console.log(err);
+        });
+    } catch{
+      console.log("nao chamaou api");
+    }
+  }
+
+  fechar() {
+    $('.modal-open').prop('checked', false);
+  }
+
 
 }
